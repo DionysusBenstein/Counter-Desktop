05e1d2a9e65be79f30772a8dcf94318e5a6205fc
DionysusBenstein <gromovdenis962@gmail.com>
2018-07-04 08:44:01 +0300

    Add getter & setter

--- Settings.cpp
+++ Settings.cpp
@@ -11,7 +11,19 @@ void Settings::setKey(const QString &key)
     emit keyChanged(key);
 }
 
+void Settings::setValue(const QString &value)
+{
+    this->value = value;
+    emit valueChanged(key);
+}
+
 QString Settings::getKey() const
 {
     return this->key;
 }
+
+QString Settings::getValue() const
+{
+    return this->value;
+}
+
--- Settings.h
+++ Settings.h
@@ -10,20 +10,21 @@ class Settings : public QObject
 public:
     explicit Settings(QObject *parent = nullptr);
     void setKey(const QString &key);
+    void setValue(const QString &value);
     QString getKey() const;
-    //void setValue(const auto &value);
-    //auto getValue() const;
+    QString getValue() const;
 
 private:
     Q_PROPERTY(QString key READ getKey WRITE setKey NOTIFY keyChanged)
-    //Q_PROPERTY(int value READ getValue WRITE setValue NOTIFY valueChanged)
+    Q_PROPERTY(QString value READ getValue WRITE setValue NOTIFY valueChanged)
 
     QString key;
+    QString value;
     QSettings settings;
 
 signals:
     void keyChanged(QString);
-    void valueChanged();
+    void valueChanged(QString);
 };
 
 #endif //SETTINGS_H
